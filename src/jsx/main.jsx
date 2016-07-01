13873b01b9d26b9ab9bfa5c278deee42ac9b80ab
cocobeezy <saguilar1991@gmail.com>
2016-07-01 13:05:34 -0700

    initial commit for new branch

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link } from 'react-router';
 import Hammer from 'react-hammerjs';
 
+require('../sass/index.scss');
+
 const Main = (props) => {
 
 	return (
 		<div className="app-container">
 			<h1 className="app-container-title">REACT STARTER KIT</h1>
-			<img className="app-container-image" src="./images/minime.png" alt="minime" />
+			<img className="app-container-image" src="./src/images/minime.png" alt="minime" />
 			{props.children}
 		</div>
 		);
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,35 @@
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
   entry: [
-    './jsx/app.jsx'
+    './src/jsx/app.jsx'
   ],
   output: {
-    path: __dirname + '/js/',
-    publicPath: '/js/',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
     filename: 'bundle.js'
   },
   module: {
     loaders: [{
       exclude: /node_modules/,
       loader: 'babel'
-    }]
+    },
+    {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      }]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ],
   devServer: {
     historyApiFallback: true,
     contentBase: './'
