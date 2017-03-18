e64e4ea52ab3012253a11352788e36471388fe86
cocobeezy <saguilar1991@gmail.com>
2017-03-17 18:20:33 -0700

    fixed type in webpack entry point

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: [
-    './src/components/app.jsx'
+    './src/components/app.js'
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
